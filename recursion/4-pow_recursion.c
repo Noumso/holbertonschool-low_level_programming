34dd8405544cbe24c2d7b280a26018ca4cab3ab3
Noumso <noumane.82300@gmail.com>
2024-11-08 16:20:31 +0100

    recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion ¬ Write a function that returns the value of x
+ * raised to the power of y
+ * @x: value to be manipulated
+ * @y: manipulator
+ *
+ * Return: sum as int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (!y)
+		return (1);
+	return (x * _pow_recursion(x, --y));
+}
