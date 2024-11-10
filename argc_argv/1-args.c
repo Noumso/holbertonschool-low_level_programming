d07d5045aee6177b6281f7f576d119d702b3cdbd
Noumso <noumane.82300@gmail.com>
2024-11-10 22:30:44 +0000

    arg

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+/**
+ * main - print the nb of arg
+ * @argc: nm of cmd line arg
+ * @argv: array that contains prgm cmd lin arg
+ * Return: 0 -success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d/n", argc - 1);
+	return (0);
+}
+
