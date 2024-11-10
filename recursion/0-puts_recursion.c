ebc088707588e753863f93e63e196672931d4bac
Noumso <noumane.82300@gmail.com>
2024-11-10 16:38:33 +0000

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion ¬ main prints a string followed by a new line
- * @s: string to print inside
+ * _puts_recursion ¬ prints a string followed by a new line.
+ * @s: pointer to char
  *
  * Return: Nothing
  */
