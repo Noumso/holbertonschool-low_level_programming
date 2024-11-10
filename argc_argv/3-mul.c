7bd2b4a6d0f6a6446ae1b004c89fc3e6478c8aaf
Noumso <noumane.82300@gmail.com>
2024-11-10 23:53:41 +0100

    arg

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two nb
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0
+ */
+
+int main(in argc, char *argv[])
+{
+	int prod;
+
+	if (argc == 3)
+	{
+		prod = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", prod);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
