8d94873e93ce16ee2946af1a6f8dc53f004bd9d9
Noumso <noumane.82300@gmail.com>
2024-11-10 16:28:35 +0000

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,23 +8,23 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt_helper(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
- * _sqrt_helper - Recursive helper function to find the square root
+ * _sqrt - Recursive helper function to find the square root
  * @n: The number to calculate the square root for
  * @i: The current number to test as a potential square root
  *
  * Return: The natural square root of n, or -1 if it doesn't exist
  */
-int _sqrt_helper(int n, int i)
+int _sqrt(int n, int i)
 {
 	if (n < 0)
 		return (-1);
-	if (i * i > n)
+	if ((i * i) > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (_sqrt_helper(n, i + 1));
+	return (_sqrt(n, i + 1));
 }
