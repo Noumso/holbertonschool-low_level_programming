02d64e13d414fe19bae6306146ddc626a94fd1aa
Noumso <noumane.82300@gmail.com>
2024-11-14 14:19:54 +0000

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,13 +12,13 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	int len1 = 0, len 2 = 0, i = 0, j = 0;
+	int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*(s1 + j))
+	while (*(s1 + i))
 		len1++, i++;
 	while (*(s2 + j))
 		len2++, j++;
