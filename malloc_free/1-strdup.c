a600ab6f70d4d87ef1660490a35e8f38a7b26186
Noumso <noumane.82300@gmail.com>
2024-11-14 13:52:18 +0000

    ram

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,36 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * 
+ * _strdup - returns a ptr to a new alloc space in memory
+ * @str: string to duplicate
+ * Return: ptr to duplicate str in alloc memory
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate_str;
+	int i = 0, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i))
+		len++, i++;
+	len++;
+
+	duplicate_str = malloc(sizeof(char) * len);
+
+	if (duplicate_str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
+	}
+
+	return (duplicate_str);
+}
