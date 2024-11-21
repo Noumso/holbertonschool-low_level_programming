24a884b92b83852e9bd76d4d5cdd48232abefcdd
Noumso <noumane.82300@gmail.com>
2024-11-21 14:47:39 +0000

    f ptr

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
 #include "3-calc.h"
 
 /**
- * main - calculate a operation
- * print sum, difference, product, dividend or remainder
+ * main - calculate operator
  * @argc: argument counter
- * @argv: argument
- * Return: 0 (succes)
+ * @argv: arguments
+ * Return: 0 on sucess
  */
-int main(argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
 	int n1, n2;
 	int (*f)(int, int);
@@ -20,7 +20,7 @@ int main(argc, char *argv[])
 		exit(98);
 	}
 
-	n1 = atoi(argv[1];
+	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
@@ -29,13 +29,13 @@ int main(argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '\' || argv[2][0] == '%') && argv[3][0] == '\0')
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 	{
-		print("Error\n");
-		exit(110);
+		printf("Error\n");
+		exit(100);
 	}
 
-	printf("%\n", f(n1, n2));
+	printf("%d\n", f(n1, n2));
 
 	return (0);
 }
