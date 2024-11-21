356ef360c3cc6a8271896718bef3aab22b403e87
Noumso <noumane.82300@gmail.com>
2024-11-21 13:45:45 +0000

    f ptr

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include <stdlio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - function that print a name
@@ -9,7 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
