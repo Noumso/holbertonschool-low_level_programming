7791923b91b03855b7c599b85536a4e1f8502f5f
Noumso <noumane.82300@gmail.com>
2024-12-05 12:24:07 +0000

    task0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,26 @@
-#include "list.h"
+#include "lists.h"
+
 /**
- * print_list - print the element in a list
- * @h: the list
+ * print_list - Prints the elements in a linked list.
+ * @h: Pointer to the head of the list.
  *
- * Description: print the element of one list
- * Return: the number of node in the list
+ * Description: Iterates through a singly linked list and prints each element.
+ *
+ * Return: The number of nodes in the list.
  */
 
 size_t print_list(const list_t *h)
 {
-	int i;
+	size_t i;
 
-	for (i = 0; h ; i++)
+	for (i = 0; h; i++)
 	{
-		if (h->str == '\0')
-			printf"[0] (nil)\n";
-		else
-			printf("[%i] %s\n", h->len, h->str);
-		h = h->next;
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
 	}
 
 	return (i);
 }
-
