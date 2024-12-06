37201df8c84ebcc8133e751484502bec009a0825
Noumso <noumane.82300@gmail.com>
2024-12-06 19:01:32 +0000

    task3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - set a bit to 1
+ * @n: the number
+ * @index: the position to set the bit
+ * Description: set 1 bit to 1
+ * Return: 1 in success -1 in fail
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	*n += 1 << index;
+	return (1);
+}
